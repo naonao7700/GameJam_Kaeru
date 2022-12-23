8a96b8e6f91c9ea40ece780469a3577c298d445f
Toshiki_Kanazawa <730730to@gmail.com>
2022-12-23 09:59:39 +0900

    no message

---
+++ Assets/Stage_2/GameClear/Goal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
